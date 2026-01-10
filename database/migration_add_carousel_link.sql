9e30c4785f746d36a7df746dd109eb2b829cbd3f
Savvas Tzanetis <94377331+stzanetis@users.noreply.github.com>
2026-01-10 14:57:12 +0200

    Server test (#1)
    * Server and image upload test
    
    * Add /setup to .gitignore
    
    * Changed default carousel image and removed unused code

--- database/migration_add_carousel_link.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Add link_url column to carousel_images table
--- Date: 2026-01-07
--- Description: Adds optional link_url field to carousel images for clickable carousel slides
-
-ALTER TABLE public.carousel_images
-ADD COLUMN IF NOT EXISTS link_url TEXT;
-
-COMMENT ON COLUMN public.carousel_images.link_url IS 'Optional URL that the carousel image links to when clicked. Can be internal (/products) or external (https://...)';
